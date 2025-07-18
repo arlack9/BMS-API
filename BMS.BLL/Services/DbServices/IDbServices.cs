7fe7ecb7eadff07f7a82d550d256284a239b7226
arlack9 <alarjun504@gmail.com>
2025-07-18 06:30:22 +0530

    go

---
+++ BMS.BLL/Services/DbServices/IDbServices.cs
@@ -0,0 +1,24 @@
+﻿using BMS.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMS.BLL.Services.DbServices;
+
+public interface IDbServices<T> where T : class
+{
+
+    public Task AddBook(T entity);
+    public Task DeleteBook(int id);
+    public Task UpdateBook(T entity);
+    public Task<IEnumerable<T>> ViewAllBooks();
+    public Task<Book> ViewBook(int id);
+
+    public event Action<Book> BookoperationSucceeded;
+
+    public event Action<int> BookDeletionSucceeded;
+
+    public event Action<Book, int> ValidationFailed;
+}
